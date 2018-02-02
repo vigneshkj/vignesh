a86d968dd2486ada863e8b60687866f8781ff9de
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-02 16:31:32 +0530

    vowel.c
    to find the vowel;

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int main()
+{
+char c;
+int isLowercaseVowel, isUppercaseVowel;
+printf("Enter an alphabet");
+scanf("%c",&c);
+printf("%c is a vowel.", c);
+else
+printf("%c is a consonant.", c);
+return 0;
+}
