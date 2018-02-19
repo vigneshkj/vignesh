1063e879d431a44781728f1e4e68848c680ae932
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-19 16:04:42 +0530

    low and high set 5.c
    to find low and high

---
+++ low and high set 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the value");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+printf("%d is high");
+}
+else
+{
+printf("%d is low");
+}
+}
