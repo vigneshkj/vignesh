442ffa93df0bf5229093e0e94932ee378918477d
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-19 16:38:16 +0530

    opreater.c
    to

---
+++ opreater.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+# bitwise
+num1=raw_input("enter the num:")
+num2=raw_input("enter the num:")
+num1=num1^num2
+num2=num1^num2
+num1=num1^num2
+print(num1)
+print(num2)
+getch();
+}
