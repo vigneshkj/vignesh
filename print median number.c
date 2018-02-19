9455662a016bf06c29773df00924d759330be69b
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-19 16:31:55 +0530

    print median number.c
    to

---
+++ print median number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+n=input("enter n value\n")
+sdd=[]
+for i in range(0,n):
+b=int(input())
+sdd.append(b)
+sdd.sort()
+if(n%2 != 0):
+print(sdd[(n/2)])
+else:
+s=n/2
+c=(int(sdd[s])+int(sdd[s-1]))/2
+print(c)
+getch();
+}
