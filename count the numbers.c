1cf3269dc5662a87f63f03b300bd9c629dea3170
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-21 16:35:33 +0530

    count the numbers.c
    count the numbers.c

---
+++ count the numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(void) {
+	char a[100];
+	gets(a);
+	int i,count=1;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
