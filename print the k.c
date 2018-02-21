bc47981e402c12e85760b7c4e404a14288f1ead8
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-02-21 16:31:35 +0530

    print the k.c
    print the k.c

---
+++ print the k.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#incude<conio.h>
+
+int main(void) 
+{
+	char a[10];
+	scanf("%s",a);
+	int n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%c",a[i]);
+	}
+	return 0;
+}
