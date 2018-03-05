20625bd6e6efd8330e93c17d71b01bd83953e35c
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-03-05 16:47:51 +0530

    product of digits.c
    product of digits.cproduct of digits.cproduct of digits.cproduct of digits.c

---
+++ product of digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+int n,a,product=1;
+scanf("%d",&n);
+while(n!=0)
+{
+a=n%10;
+product=product*a;
+n=n/10;
+}
+printf("%d",product);
+return 0;
+}
