f32bcfd21f406ad1f98021cefab9dd6fb1ee8294
vigneshkj <35889254+vigneshkj@users.noreply.github.com>
2018-03-07 15:46:52 +0530

    every word.c
    every word.cevery word.cevery word.cevery word.cevery word.cevery word.cevery word.cevery word.cevery word.cevery word.c

---
+++ every word.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+int i;
+char str[100];
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if(i==0 )
+{
+if(str[i]>='a' && str[i]<='z')
+{
+str[i]=str[i]-32;
+}
+}
+if(str[i]==' ')
+{
+++i;
+if(str[i]>='a' && str[i]<='z')
+{
+str[i]=str[i]-32;
+}
+}
+}
+printf("%s",str);
+return 0;
+}
